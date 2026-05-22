3e19ddda22c67ec4c245578174ce605c6a3a8d98
Itay Mor(איתי מור) <itay313122@gmail.com>
2026-05-22 15:36:44 +0300

    fix: remove --single-process puppeteer flag

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ const client = new Client({
       '--disable-accelerated-2d-canvas',
       '--no-first-run',
       '--no-zygote',
-      '--single-process',
       '--disable-gpu',
     ],
   },
